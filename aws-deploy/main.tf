428df57964f397d48ed22fbb822d9da23892e4e2
Prasanjit Singh <mailprasanjit@gmail.com>
2020-10-05 03:26:50 +0400

    Update main.tf

--- aws-deploy/main.tf
+++ aws-deploy/main.tf
@@ -5,10 +5,10 @@ provider "aws" {
 resource "aws_instance" "web" {
 
   ami           = "lookup(var.ami,var.aws_region)"
-  instance_type = "${var.instance_type}"
-  vpc_security_group_ids = ["${aws_security_group.web.id}"]
-  user_data              = "${file("template/user_data.sh")}"
-  instance_count                  = "${var.instance_count}"
+  instance_type = "var.instance_type"
+  vpc_security_group_ids = ["aws_security_group.web.id"]
+  user_data              = "file("template/user_data.sh")"
+  instance_count                  = "var.instance_count"
 
 
   tags {
